52d69c77323ab7a663f8af3ba369343d131c7c22
Alexandre Coletti <eu_axil@yahoo.com.br>
2024-01-12 19:02:29 -0300

    adicionando projeto de TS com Express

---
+++ 13_ts_com_express/comandos.ts
@@ -0,0 +1,18 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++ iniciando projeto
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+//!para inciar e criar a package.json (-y para não precisar responder questões e ele criar padrão)
+//npm init -y 
+
+//!para criar o tsconfig.json e iniciar o 'typescript'
+//npx tsc --init
+
+//!para instalar o express
+//npm install @types/express @types/node ts-node-dev typescript --save-dev
+//npm install express
+
+
+//!criado no arquivo package.json, abaixo de test (com isso pode-se rodar a aplicação com 'npm run dev'):
+//"dev": "ts-node-dev --respawn --transpile-only src/app.ts"=
