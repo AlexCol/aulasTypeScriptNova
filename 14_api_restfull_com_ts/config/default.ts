eecd6c6db729c08f6383d49346c705a158e8347c
Alexandre Coletti <eu_axil@yahoo.com.br>
2024-02-11 14:45:56 -0300

    adicionado informação sobre a string de conexão

--- 14_api_restfull_com_ts/config/default.ts
+++ 14_api_restfull_com_ts/config/default.ts
@@ -3,6 +3,7 @@ const dbPassword = process.env.DB_PASS;
 
 export default {
     port: 3000,
-    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@restapimongodb.jq4pewc.mongodb.net/myFirstDB?retryWrites=true&w=majority`,
+    //!para escolher qual o banco usar, e não criar um 'test', especificar na conection string, após o '.net' o nome do banco
+	dbUri: `mongodb+srv://${dbUser}:${dbPassword}@restapimongodb.jq4pewc.mongodb.net/myFirstDB?retryWrites=true&w=majority`,
     env: "development"
 };
